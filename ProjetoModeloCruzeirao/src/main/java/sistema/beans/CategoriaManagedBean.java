d38bcc93c430ccb82018be1dc2f2030c8c5121d7
Victor Marun <Victor Marun@DESKTOP-D7H3V70>
2017-04-09 17:32:22 -0300

    teste

--- ProjetoModeloCruzeirao/src/main/java/sistema/beans/CategoriaManagedBean.java
+++ ProjetoModeloCruzeirao/src/main/java/sistema/beans/CategoriaManagedBean.java
@@ -5,6 +5,7 @@ import java.util.List;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 
+import sistema.modelos.Campeonato;
 import sistema.modelos.Categoria;
 import sistema.service.CategoriaService;
 
@@ -15,10 +16,15 @@ public class CategoriaManagedBean {
 	private int codigoCategoria = 1;
 	private Categoria categoria = new Categoria(codigoCategoria);
 	private Categoria categoriaAtual;
+	private CampeonatoManagedBean campeonato;
 	private CategoriaService service = new CategoriaService();
 	
+	
+	
+
 	public void salvar()
 	{
+		
 		service.salvar(categoria);
 		codigoCategoria++;
 		categoria = new Categoria(codigoCategoria);
